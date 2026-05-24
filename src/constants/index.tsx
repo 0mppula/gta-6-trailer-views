528332b0704797d605f98246428e9b157c43734d
Omar Kraidié <omar.kraidie@vincit.fi>
2026-05-24 11:26:54 +0300

    Update labels for GTA V trailer video IDs for accuracy and consistency

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -16,46 +16,46 @@ export const GTA_5_TRAILER_VIDEO_IDS = [
 	},
 	{
 		id: 'N-xHcvug3WI',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V: Official Gameplay Video',
 	},
 	{
 		id: 'hvoD7ehZPcM',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V: The Official Trailer',
 	},
 	{
 		id: 'Bf38HiYPMiI',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V: Michael. Franklin. Trevor.',
 	},
 	{
 		id: '3DBrG2YjqQA',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V: PlayStation 4, Xbox One & PC Announcement Trailer',
 	},
 	{
 		id: 'olEGtoYs_8A',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto Online: Official Gameplay Video',
 	},
 	{
 		id: 'rb1vOrm9SX4',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V: “A Picket Fence and a Dog Named Skip”',
 	},
 	{
 		id: 'VNbONMSObfs',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V and Grand Theft Auto Online for PS5 and Xbox Series X|S – Coming March 2022',
 	},
 	{
 		id: '-HIv_Uj1ERE',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V – Available on the Rockstar Games Launcher',
 	},
 	{
 		id: 'lqiZdynJmag',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V and GTA Online Out Now on PlayStation 5 and Xbox Series X|S',
 	},
 	{
 		id: 'dtJj0So2yq4',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Experience the Best Version of GTAV and GTA Online on PC with GTAV Enhanced',
 	},
 	{
 		id: 'SbC1etgVNZk',
-		label: 'Grand Theft Auto V Trailer',
+		label: 'Grand Theft Auto V - Xbox 360 TV Spot',
 	},
 ];
